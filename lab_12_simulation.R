d2975dd930a2b7a78a50669bec2a2ce29317328b
John Curcio <jmarrvin@gmail.com>
2018-04-20 21:26:23 -0400

    modified model_select because I misread the prompt

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -9,10 +9,26 @@ generate_data <- function(n, p){
 
 # 2b
 model_select <- function(covariates, responses, cutoff){
+  p_val_colname <- "Pr(>|t|)"
   mod <- lm(responses ~ covariates)
-  p_vals <- summary(mod)$coefficients[, "Pr(>|t|)"]
+  p_vals <- summary(mod)$coefficients[, p_val_colname]
   sig_inds <- which(p_vals <= cutoff)
-  if(length(sig_inds) > 0) return(lm(responses ~ covariates[,sig_inds]))
-  
-  return(lm(responses ~ 1))
+  if(length(sig_inds) > 0){
+    sig_mod <- lm(responses ~ covariates[,sig_inds])
+    sig_p_vals <- summary(sig_mod)$coefficients[, p_val_colname]
+    return(sig_p_vals)
+  } else{
+    return(c())
+  }
 }
+
+# 2c
+# run_simulation <- function(n_trials, n, p, cutoff){
+#   datasets <- lapply(1:n_trials, 
+#                      function(i){
+#                        data <- generate_data(n, p)
+#                        
+#                        })
+#   
+# }
+
